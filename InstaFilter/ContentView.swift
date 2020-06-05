572d7566d787f2f2186c915abda6585d4a91d2ad
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2020-06-05 15:55:11 -0400

    Day 62 - "Showing multiple options with ActionSheet"

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -9,23 +9,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount : CGFloat = 0
+    @State private var showingActionSheet = false
+    @State private var backgroundColor = Color.white
     
     var body: some View {
-        let blur = Binding<CGFloat> (
-            get: {
-                self.blurAmount
-        },
-            set: { nummy in
-                self.blurAmount = nummy
-                print("new blur=\(self.blurAmount)")
+        Text("Let's change our background color!")
+            .frame(width: 300, height: 300)
+            .font(.largeTitle)
+            .background(backgroundColor)
+            .onTapGesture {
+                self.showingActionSheet = true
         }
-        )
-        return VStack {
-            Text("Hello, World!")
-                .blur(radius: blurAmount)
-            
-            Slider(value: blur, in: 0...20)
+        .actionSheet(isPresented: $showingActionSheet) {
+            ActionSheet(title: Text("My first action sheet"), message: Text("Pick a background color"), buttons: [
+                .default(Text("blue")) {
+                    self.backgroundColor = Color.blue
+                },
+                .default(Text("red")) {
+                    self.backgroundColor = Color.red
+                },
+                .default(Text("orange")) {
+                    self.backgroundColor = Color.orange
+                },
+                .default(Text("yellow")) {
+                    self.backgroundColor = Color.yellow
+                },
+                .cancel()
+            ])
         }
     }
 }
