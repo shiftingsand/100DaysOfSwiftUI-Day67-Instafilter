b68e4cbfaef4963ba2c43ed3685da7edaacb9065
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2020-06-06 17:37:29 -0400

    Day 63 - "Integrating Core Image with SwiftUI" part 3

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -12,9 +12,14 @@ import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @State private var image : Image?
+    @State private var flipper = true
     
     var body: some View {
         VStack {
+            Button("Press me") {
+                self.flipper.toggle()
+                self.loadImage()
+            }
             image?
                 .resizable()
                 .scaledToFit()
@@ -30,12 +35,24 @@ struct ContentView: View {
         let beginImage = CIImage(image: inputImage)
         
         let context = CIContext()
-        let currentFilter = CIFilter.sepiaTone()
-        currentFilter.inputImage = beginImage
-        currentFilter.intensity = 1
+        var finalImage : CIFilter
+        
+        if flipper {
+            let currentFilter = CIFilter.sepiaTone()
+            currentFilter.inputImage = beginImage
+            currentFilter.intensity = 1
+            finalImage = currentFilter
+        } else {
+            // twirl hasn't been updated and you have to use old way
+            guard let currentFilter = CIFilter(name: "CITwirlDistortion") else { return }
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            currentFilter.setValue(2000, forKey: kCIInputRadiusKey)
+            currentFilter.setValue(CIVector(x: inputImage.size.width / 2, y: inputImage.size.height / 2), forKey: kCIInputCenterKey)
+            finalImage = currentFilter
+        }
         
         // returns CIImage
-        guard let outputImage = currentFilter.outputImage else {
+        guard let outputImage = finalImage.outputImage else {
             return
         }
         
