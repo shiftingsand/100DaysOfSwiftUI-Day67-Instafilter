70089103b508c56eb0548de071b15abaef4a8057
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2020-06-06 15:54:11 -0400

    Day 63 - "Integrating Core Image with SwiftUI" part 1

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -9,34 +9,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingActionSheet = false
-    @State private var backgroundColor = Color.white
+    @State private var image : Image?
+    //@State private var backgroundColor = Color.white
     
     var body: some View {
-        Text("Let's change our background color!")
-            .frame(width: 300, height: 300)
-            .font(.largeTitle)
-            .background(backgroundColor)
-            .onTapGesture {
-                self.showingActionSheet = true
+        VStack {
+            image?
+            .resizable()
+            .scaledToFit()
         }
-        .actionSheet(isPresented: $showingActionSheet) {
-            ActionSheet(title: Text("My first action sheet"), message: Text("Pick a background color"), buttons: [
-                .default(Text("blue")) {
-                    self.backgroundColor = Color.blue
-                },
-                .default(Text("red")) {
-                    self.backgroundColor = Color.red
-                },
-                .default(Text("orange")) {
-                    self.backgroundColor = Color.orange
-                },
-                .default(Text("yellow")) {
-                    self.backgroundColor = Color.yellow
-                },
-                .cancel()
-            ])
-        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        image = Image("ookla")
     }
 }
 
