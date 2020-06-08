e3b6f7435e2f5c17e6da9ce72607a4d08dfe5e69
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2020-06-08 13:08:52 -0400

    Day 65 - "Basic image filtering using Core Image"

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -15,54 +15,82 @@ struct ContentView: View {
     @State private var inputImage: UIImage?
     @State private var filterIntensity = 0.5
     @State private var showingImagePicker = false
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     var body: some View {
-        VStack {
-            ZStack {
-                Rectangle()
-                    .fill(Color.secondary)
-                
-                if nil != image {
-                    image?
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    Text("Tap to select a picture")
-                        .foregroundColor(.white)
-                        .font(.headline)
-                }
-            }
-            .onTapGesture() {
-                self.showingImagePicker = true
-            }
-            
-            HStack {
-                Text("Intensity")
-                Slider(value: self.$filterIntensity)
-            }.padding(.vertical)
-            
-            HStack {
-                Button("Change filter") {
-                    //self.showImagePicker = true
-                }
-                
-                Spacer()
-                
-                Button("Save") {
+        let intensity = Binding<Double> (
+            get: {
+                self.filterIntensity
+        },
+            set: { nummy in
+                self.filterIntensity = nummy
+                self.applyProcessing()
+        }
+        )
+        
+        return NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.secondary)
                     
+                    if nil != image {
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Text("Tap to select a picture")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
                 }
+                .onTapGesture() {
+                    self.showingImagePicker = true
+                }
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: intensity)
+                }.padding(.vertical)
+                
+                HStack {
+                    Button("Change filter") {
+                        //self.showImagePicker = true
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Save") {
+                        
+                    }
+                }
+            } // vstack
+                .padding([.horizontal, .bottom])
+                .navigationBarTitle("Instafilter")
+                .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+                    ImagePicker(image: self.$inputImage)
             }
         }
-        .padding([.horizontal, .bottom])
-        .navigationBarTitle("Instafilter")
-        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-            ImagePicker(image: self.$inputImage)
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
         }
     }
     
     func loadImage() {
         guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+        
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
     }
 }
 
